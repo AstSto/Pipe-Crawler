bae40203a5f204ef12f4c9616f264ec421a6d2c9
AstSto <stock_astrid@yahoo.de>
2017-05-19 18:31:24 +0200

    Add files via upload
    Arduino Programm der bisherigen Regelung un Bluetoothübertragung

---
+++ hbruecke_BT.ino
@@ -0,0 +1,170 @@
+//**********************SETUP L293D
+//Motor A
+const int motorPin1  = 5;  // Pin 14 of L293
+const int motorPin2  = 6;  // Pin 10 of L293
+//Motor B
+const int motorPin3  = 10; // Pin  7 of L293
+const int motorPin4  = 9;  // Pin  2 of L293
+
+
+
+//**********************SETUP MPU
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#include <string.h>
+#include<stdlib.h>
+#endif
+#include <SoftwareSerial.h>
+SoftwareSerial bluetooth(0, 1); //RX, TX
+
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+
+void setup() {
+  //*******************SETUP DIAGNOSE***************************
+  Serial.begin(9200);
+
+  //***********************SETUP MOTOR*******************************************
+  //Set pins as outputs
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+
+  //Motor Control - Motor A: motorPin1,motorpin2 & Motor B: motorpin3,motorpin4
+
+  //This code  will turn Motor A clockwise for 2 sec.
+
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin4, LOW);
+
+  //***********************SETUP MPU***********************************************
+
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  //turn on DMP
+  mpu.setDMPEnabled(true);
+
+  // enable Arduino interrupt detection
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+  mpuIntStatus = mpu.getIntStatus();
+
+  // set our DMP Ready flag so the main loop() function knows it's okay to use it
+  dmpReady = true;
+
+  // get expected DMP packet size for later comparison
+  packetSize = mpu.dmpGetFIFOPacketSize();
+}
+
+
+void loop() {
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    // get Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    ypr[0] * 180 / M_PI;
+    ypr[1] * 180 / M_PI;
+    ypr[2] * 180 / M_PI;
+#endif
+
+  }//else if
+
+  analogWrite(motorPin3, 64 + (100 * ypr[1]));
+  analogWrite(motorPin1, 64 - (100 * ypr[1]));
+  //Serial.println(ypr[0] * 180 / 2);           //nicht mit Bluetoothverwendung anzeigen lassen
+  //Serial.println(ypr[1] * 180 / 2);
+  //Serial.println(ypr[2] * 180 / 2);
+
+
+------------------------------BLUETOOTH-ÜBERTRAGUNG-----------------------------------------------------------------------------
+
+char test[20];
+String a = dtostrf(ypr[0],4,3,test);      //floatToString(buffer string, float value, precision, minimum text width)
+String b = dtostrf(ypr[1],4,3,test);
+String c = dtostrf(ypr[2],4,3,test);
+String senden = String(a+"q"+b+"q"+c);
+
+//bluetooth.println("Start"+senden);
+Serial.println(senden); // HURRRA!!!!!
+
+ /* //bluetooth.print("START,");
+    for (int i = 0; i < 3; i++) {
+     bluetooth.print(String(ypr[i] * 180 / 2));
+     bluetooth.print("q");
+    }
+    bluetooth.println("");*/
+
+}//loop
+
+
